edc35935b233dedb2db5536bfe27458fb262d0c5
Guerrito20 <driinkssz@gmail.com>
2023-01-17 03:20:38 -0300

    adding minus and plus buttons

--- script.js
+++ script.js
@@ -1,9 +1,60 @@
 const botao = document.querySelector("#botao");
+const resetbtn = document.getElementById("reset");
+const display = document.getElementById("resultado-final");
+const defaultmin = 1;
+const defaultmax = 50;
+const defaultv = 0;
+
+let min1 = parseInt(document.getElementById("Min").value);
+let max1 = parseInt(document.getElementById("Max").value);
+
+function plusminus(id) {
+  if (id == "minusMin") {
+    console.log(id);
+
+    min1--;
+    document.getElementById("Min").stepDown(1);
+
+    console.log(min1);
+  } else if (id == "maxMin") {
+    console.log(id);
+    min1++;
+    document.getElementById("Min").stepUp(1);
+
+    console.log(min1);
+  } else if (id == "minusMax") {
+    console.log(id);
+    max1--;
+    document.getElementById("Max").stepDown(1);
+
+    console.log(max1);
+  } else if (id == "maxMax") {
+    console.log(id);
+    max1++;
+    document.getElementById("Max").stepUp(1);
+
+    console.log(max1);
+  }
+}
+
+resetbtn.onclick = function reset() {
+  display.innerHTML = defaultv;
+  min1 = defaultmin;
+  max1 = defaultmax;
+
+  console.log(display, min1, max1);
+};
+
+/* function diminuir(id) {
+  if ((this.id = minusMin)) {
+    min1 - 1;
+    console.log(min1.value);
+  }
+} */
 
 botao.onclick = function ativar() {
-  const display = document.getElementById("resultado-final");
-  let min1 = parseInt(document.getElementById("Min").value);
-  let max1 = parseInt(document.getElementById("Max").value);
+  min1 = parseInt(document.getElementById("Min").value);
+  max1 = parseInt(document.getElementById("Max").value);
 
   if (min1 > max1) {
     window.alert(
